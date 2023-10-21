76c4202feade0275ec79b575ef330a60d70ed36e
Hegazy02 <Abdelrhmanhegazy02@gmail.com>
2023-10-21 20:16:36 +0300

    display data on searchView

--- lib/features/search/presentation/view/search_view.dart
+++ lib/features/search/presentation/view/search_view.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_bookly/core/utils/service_locator.dart';
+import 'package:my_bookly/features/search/data/repos/search_repo.dart';
 
 import 'package:my_bookly/features/search/presentation/view/widgets/search_view_body.dart';
+import 'package:my_bookly/features/search/presentation/view_model/search/search_cubit.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: SearchViewBody()));
+    return Scaffold(
+        body: SafeArea(
+            child: BlocProvider(
+      create: (context) => SearchCubit(ServiceLocator.getIt<Search>()),
+      child: const SearchViewBody(),
+    )));
   }
 }
--- lib/features/search/presentation/view/widgets/search_view_body.dart
+++ lib/features/search/presentation/view/widgets/search_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_bookly/core/utils/assets_paths.dart';
 import 'package:my_bookly/core/utils/service_locator.dart';
+import 'package:my_bookly/features/home/data/models/new_book_model/book_model.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/new_books_list.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/new_books_item.dart';
 import 'package:my_bookly/features/search/data/repos/search_repo.dart';
@@ -19,68 +21,68 @@ class _SearchViewBodyState extends State<SearchViewBody> {
   String? searchValue;
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SearchCubit(ServiceLocator.getIt<Search>()),
-      child: BlocBuilder<SearchCubit, SearchState>(
-        builder: (context, state) {
-          if (state is SearchSuccess) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 10,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          SearchTextField(
+            onChanged: (value) {
+              BlocProvider.of<SearchCubit>(context)
+                  .searchedBooks(searchedValue: value);
+            },
+          ),
+          BlocBuilder<SearchCubit, SearchState>(
+            builder: (context, state) {
+              if (state is SearchSuccess) {
+                return Expanded(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Search Result",
+                          style: Styles.textStyle18,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        child: CustomScrollView(
+                          physics: const BouncingScrollPhysics(),
+                          slivers: [
+                            NewBooksListWidget(
+                              booksModels: state.searchedBooks,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  SearchTextField(
-                    onChanged: (value) {
-                      setState(() {
-                        searchValue = value;
-                      });
-                    },
+                );
+              } else if (state is SearchFailure) {
+                return Center(
+                  child: Text(
+                    state.errMessage,
+                    style: Styles.textStyle18,
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Search Result",
-                      style: Styles.textStyle18,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  searchValue != null
-                      ? Expanded(
-                          child: CustomScrollView(
-                            physics: const BouncingScrollPhysics(),
-                            slivers: [
-                              NewBooksListWidget(
-                                booksModels: state.searchedBooks,
-                              ),
-                            ],
-                          ),
-                        )
-                      : const SizedBox()
-                ],
-              ),
-            );
-          } else if (state is SearchFailure) {
-            return Center(
-              child: Text(
-                state.errMessage,
-                style: Styles.textStyle18,
-              ),
-            );
-          } else if (state is SearchLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            return const SizedBox();
-          }
-        },
+                );
+              } else if (state is SearchLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return const SizedBox();
+              }
+            },
+          ),
+        ],
       ),
     );
   }
